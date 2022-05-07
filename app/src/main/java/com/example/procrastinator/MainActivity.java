8c74cab39f39e612df5d6db1a9331e138f051bd7
Jeremy Nguyen <nguyen.jeremy.minhchau@gmail.com>
2022-05-07 12:35:11 +0200

    Project init

---
+++ app/src/main/java/com/example/procrastinator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.procrastinator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
