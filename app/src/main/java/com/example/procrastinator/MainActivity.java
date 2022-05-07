648010a1e1bd2e69a63611c20b137a42fbab8f41
Jeremy Nguyen <nguyen.jeremy.minhchau@gmail.com>
2022-05-07 15:07:26 +0200

    Debug button

--- app/src/main/java/com/example/procrastinator/MainActivity.java
+++ app/src/main/java/com/example/procrastinator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.procrastinator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button_debug);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                debug(v);
+            }
+        });
+    }
+
+    public void debug(View view) {
+        Toast.makeText(this, "debug", Toast.LENGTH_SHORT).show();
     }
 }
